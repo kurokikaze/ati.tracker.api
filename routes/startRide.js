a47dce29e9f323f7b588162cefec7a39dac01e8a
Serge Shirokov <shirokov.s@ati.su>
2017-03-03 14:37:03 +0300

    Метаданные поездки, цена

--- routes/startRide.js
+++ routes/startRide.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var config = require('../bin/config.js');
-
+var debug = require('debug')('ati-tracker-api');
 
 function processStartRide(req, res, next) {
 
@@ -10,22 +10,35 @@ function processStartRide(req, res, next) {
       if (err) {
         throw err
       } else {
+        debug(req.body);
 
-        if (req.body && req.body != '') {
-          console.error(req.body);
-          //var request = JSON.parse(req.body);
-        }
-
-        var lat = req.params.lat ? req.params.lat : req.query.lat; 
-        var lon = req.params.lon ? req.params.lon : req.query.lon; 
-        var time = req.params.time ? req.params.time : req.query.time; 
+        var lat = req.body.lat ? req.body.lat : req.query.lat; 
+        var lon = req.body.lon ? req.body.lon : req.query.lon; 
+        var time = req.body.time ? req.body.time : req.query.time;
+        var money = req.body.money ? req.body.money : req.query.money;
 
         var loadId = req.params.loadid;
+
         var point = {
           "lat": lat ? lat : 12.01,
           "lon": lon ? lon : 12.02,
           "time": time ? time : Date.now()
         }
+
+        var rideObject = {
+          "loadId": loadId,
+          "status": "started",
+          "startTime": Date.now(),
+          "endTime": null,
+          "money": money ? money : 500
+        };
+
+        debug(rideObject);
+
+        db.collection('currentRides').remove({"loadId": loadId}, function(err, r) {
+          db.collection('currentRides').insertOne(rideObject);
+        });
+
         db.collection('loadid:' + loadId).remove(function(err, r){
           db.collection('loadid:' + loadId).insertOne(point);
           var answer = {};
